c6fbf574390c254de184f572de5d31c2db320af7
summer semester acc#22 <s0022@atcremers65>
2018-09-27 16:20:31 +0200

    Modified optimizer header for testing

--- src/optimizer.cuh
+++ src/optimizer.cuh
@@ -24,12 +24,14 @@ public:
     ~Optimizer();
 
 	void optimize(float* optimDeformationU, float* optimDeformationV, float* optimDeformationW, TSDFVolume* tsdfLive);
+	void test(float* optimDeformationU, float* optimDeformationV, float* optimDeformationW, TSDFVolume* tsdfLive);
 
 protected:
 	void allocateMemoryInDevice();
 	void copyArraysToDevice();
-
+	void computeGradient(float* gradOutX, float* gradOutY, float* gradOutZ, const float* tsdfLive, const float *kernelDx, const float *kernelDy, const float *kernelDz, int kradius, int w, int h, int d);
 	void computeDivergence(float* divOut, const float* deformationInU, const float* deformationInV, const float* deformationInW, const float *kernelDx, const float *kernelDy, const float *kernelDz, int kradius, int w, int h, int d);
+	void getSlice(float* sliceOut, const float* gridIn, size_t sliceInd);
 
     TSDFVolume* m_tsdfGlobal;
     float* m_deformationFieldU, * m_deformationFieldV, * m_deformationFieldW;
@@ -42,6 +44,12 @@ protected:
 	float* m_d_kernelDx = NULL;
 	float* m_d_kernelDy = NULL;
 	float* m_d_kernelDz = NULL;
+	// Gradients of live SDF
+	float* m_d_sdfDx = NULL;
+	float* m_d_sdfDy = NULL;
+	float* m_d_sdfDz = NULL;
+
+	// Gradients of deformation field
 	float* m_d_dx = NULL;
 	float* m_d_dy = NULL;
 	float* m_d_dz = NULL;
